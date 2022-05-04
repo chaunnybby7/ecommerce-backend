5509d56d1ea6829561015622f98e7e3ccbdb7633
YiLin Ong <chauntelleinc@gmail.com>
2022-05-04 18:26:29 -0400

    added get one product code on product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,8 +34,30 @@ router.get('/:id', (req, res) => {
   Product.findOne({
       where: {
         id: req.params.id
+      },
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
       }
+      ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product found with this id.'});
+      return;
   }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
